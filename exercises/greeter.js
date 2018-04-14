b6fa92813f3bd466757c5021f76235e2973a95d3
Frank <knarf56@gmail.com>
2018-04-14 13:52:17 -0400

    Added Game

--- exercises/greeter.js
+++ exercises/greeter.js
@@ -2,7 +2,7 @@ var name = prompt('What is your name?');
 
 var checkName = function(){
   if(name === "") {
-    name = prompt('What is your name, for real this time?');
+    name = prompt(`What is your name? I'm Samuel L. Jackson! Stop playing with me muthafucka!`);
 
     checkName(); // repeats until a name exists
   }
@@ -10,4 +10,4 @@ var checkName = function(){
 
 checkName(); // kicks off the name-checking the first time
 
-alert('Hello ' + name + '!');
+alert('Hello ' + name + '! ' + `It wasn't that hard, was it?`);
